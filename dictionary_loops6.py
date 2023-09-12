f42323c64489474795d63b07467c0266cff48e9c
Connor Feldman <80939702+cpfeldman64@users.noreply.github.com>
2023-09-11 20:17:21 -0500

    Add files via upload

--- dictionary_loops6.py
+++ dictionary_loops6.py
@@ -31,14 +31,64 @@ for name, language in favorite_language.items():
 favorite_foods = {
     'connor': 'pancakes',
     'austin': 'jambalaya',
-    'hope': 'grapes',
+    'hope': 'grapes', 
 }
 
-for name in favorite_foods.keys():
-    print(name.title())
+print(favorite_foods)
+
+
+for person in favorite_foods.keys():
+    print(f"\nHi {person.title()}!")
+
+
 
 # same output if written as below, because keys are the default item for listing
 # for name in favorite_food:
     #print(name.title())
 
+friends = ['austin', 'hope']
 
+for person in friends:
+    food = favorite_foods[person].title()
+    print(f"\n{person.title()}, I see you love {food}!")
+
+if 'ty' not in favorite_foods.keys():
+    print("\nTy is not my friend!")
+
+
+print("\n\n\n")
+
+# Looping through keys in particular order
+# You can sort through keys as they are returned in a for loop using 
+# the sorted function
+
+favorite_dogs = {
+    'austin': 'german shepherd',
+    'hope': 'pitbull',
+    'connor': 'doberman',
+    'ty': 'pitbull',
+    'root': 'corgi',
+}
+
+for name in sorted(favorite_dogs.keys()):
+    print(f"\n{name.title()}, thank you for taking the poll.\n")
+
+
+
+# Looping through all VALUES in a dictionary
+# Can use the VALUES() METHOD to return sequence of values
+
+print("The following dog breeds have been mentioned:")
+for dog in sorted(favorite_dogs.values()):
+    print(dog.title())
+# NOTE that looping through the values requires the .values() method,
+# unlike looping through keys, where that can be omitted.
+
+
+
+# Looping through Dictionary VALUES WITHOUT REPETITION using a SET
+# A SET is a collection in which item must be unique.
+
+print("\nThe following dog poll results were:")
+for dog in set(favorite_dogs.values()):
+    print(dog.title())
